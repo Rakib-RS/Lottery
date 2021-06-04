6188131d822c91e63cc6f67f1c6a186d32efdb31
rakib-rs <rakib.rscse@gmail.com>
2021-06-04 20:47:36 +0600

    allows one account to enter

--- test/Lottery.test.js
+++ test/Lottery.test.js
@@ -1,6 +1,6 @@
-const assert = require('assert');
-const ganache = require('ganache-cli');
-const Web3 = require('web3');
+const assert = require("assert");
+const ganache = require("ganache-cli");
+const Web3 = require("web3");
 const web3 = new Web3(ganache.provider());
 //const {interface,bytecode} = require('../compile');
 const { interface, bytecode } = require("../compile");
@@ -8,15 +8,23 @@ const { interface, bytecode } = require("../compile");
 let accounts;
 let lottery;
 
-beforeEach( async () =>{
-    accounts = await web3.eth.getAccounts();
-    lottery = await new web3.eth.Contract(JSON.parse(interface))
-    .deploy({ data: bytecode, })
+beforeEach(async () => {
+  accounts = await web3.eth.getAccounts();
+  lottery = await new web3.eth.Contract(JSON.parse(interface))
+    .deploy({ data: bytecode })
     .send({ from: accounts[0], gas: "1000000" });
-})
+});
 
-describe('Lottery Contract',() => {
-    it('deploys contract',() =>{
-        assert.ok(lottery.options.address);
-    })
-})
+describe("Lottery Contract", () => {
+  it("deploys contract", () => {
+    assert.ok(lottery.options.address);
+  });
+  it("allows one account to enter", async () => {
+    await lottery.methods
+      .enter()
+      .send({ from: accounts[0], value: web3.utils.toWei("0.02", "ether") });
+    const players = await lottery.methods.getPlayers().call({from:accounts[0]});
+    assert.equal(accounts[0],players[0]);
+    assert.equal(1,players.length)
+  });
+});
